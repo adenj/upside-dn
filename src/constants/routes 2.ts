ad92a42d00e3573d71f6b0b2658640dd61514a2e
Aden Margheriti <adennn@gmail.com>
2023-07-29 13:12:44 +1000

    delete dupe file

--- src/constants/routes 2.ts
+++
@@ -1,5 +0,0 @@
-export const ROOT_PATH = "/";
-export const FEED_PATH = "/feed";
-export const TOKEN_PATH = "/token";
-export const SAVERS_PATH = "/savers";
-export const SAVER_PATH = "/savers/:id";
