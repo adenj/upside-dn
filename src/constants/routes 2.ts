750bdf284ce9746e14736e3772471dd886de6b17
Aden Margheriti <adennn@gmail.com>
2023-07-29 11:10:47 +1000

    remove unused deps

---
+++ src/constants/routes 2.ts
@@ -0,0 +1,5 @@
+export const ROOT_PATH = "/";
+export const FEED_PATH = "/feed";
+export const TOKEN_PATH = "/token";
+export const SAVERS_PATH = "/savers";
+export const SAVER_PATH = "/savers/:id";
